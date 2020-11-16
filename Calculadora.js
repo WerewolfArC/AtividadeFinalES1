a6bf5e1dd866f1d1e3fd122709439033166f54a5
EduadoSoares <edu.s.soares@hotmail.com>
2020-11-16 19:53:21 -0300

    Adds multiplication functionality

--- Calculadora.js
+++ Calculadora.js
@@ -8,6 +8,14 @@ function sub(){
 
 function mult(){
     //TODO: Implementar multiplicação
+    let X = prompt(Number("Primeiro número: "))
+    let Y = prompt(Number ("Segundo número: "))
+
+    let A = X*Y
+    let B = Y*X
+
+    console.log(`${X} x ${Y} = ${A.toFixed(2)}`)
+    console.log(`${Y} x ${X} = ${B.toFixed(2)}`)
 }
 
 function div(){
