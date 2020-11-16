620aa4cd79aa024b67e7f91fedc5cbd6d55e7e31
EduardoSoares <edu.s.soares@hotmail.com>
2020-11-16 02:03:20 -0300

    Adds subtraction functionality

--- Calculadora.js
+++ Calculadora.js
@@ -14,6 +14,14 @@ function sum(){
 
 function sub(){
     //TODO: Implementar subtração
+    let x = prompt(Number("Primeiro número: "))
+    let y = prompt(Number("Segundo número: "))
+
+    let a = x-y
+        console.log(`${x} - ${y} = ${a.toFixed(2)}`)
+
+    let b = y-x
+        console.log(`${y} - ${x} = ${b.toFixed(2)}`)
 }
 
 function mult(){
