c0a24d8f3fbf68939b8cc306e7e40692a7c17e07
Arthur Gomes de Freitas Cardozo <64204286+WerewolfArC@users.noreply.github.com>
2020-11-16 21:19:38 -0300

    Atualizado para padrão adotado.

--- Calculadora.js
+++ Calculadora.js
@@ -20,6 +20,14 @@ function mult(){
     console.log(`${Y} x ${X} = ${B.toFixed(2)}`)
 }
 
-function div(dividendo = new Number, divisor = new Number){
-    return dividendo/divisor
+function div(dividendo = new Number, divisorfunction div(){
+    //TODO: Implementar divisão
+    let X = prompt(Number("Primeiro número: "))
+    let Y = prompt(Number ("Segundo número: "))
+
+    let A = X/Y
+    let B = Y/X
+
+    console.log(`${X} / ${Y} = ${A.toFixed(2)}`)
+    console.log(`${Y} / ${X} = ${B.toFixed(2)}`)
 }
