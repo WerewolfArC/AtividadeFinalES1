81571344f2818dc718cbb284d858b19c520933a8
Arthur Gomes de Freitas Cardozo <64204286+WerewolfArC@users.noreply.github.com>
2020-11-16 21:27:27 -0300

    Adds sum functionality

--- Calculadora.js
+++ Calculadora.js
@@ -2,6 +2,14 @@ var prompt = require('prompt-sync')();
 
 function sum(){
     //TODO: Implementar soma
+    let X = prompt(Number("Primeiro número: "))
+    let Y = prompt(Number ("Segundo número: "))
+
+    let A = X+Y
+    let B = Y+X
+
+    console.log(`${X} x ${Y} = ${A.toFixed(2)}`)
+    console.log(`${Y} x ${X} = ${B.toFixed(2)}`)
 }
 
 function sub(){
